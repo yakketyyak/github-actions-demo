a221d1042f8cac1e14be1f608894f6ac2d540b00
Gnongo Patrick <gpbeugre-ext@airfrance.fr>
2021-08-24 11:19:18 +0200

    init actions

---
+++ src/test/java/com/yakketyyak/github/actions/GithubActionsDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yakketyyak.github.actions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubActionsDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
